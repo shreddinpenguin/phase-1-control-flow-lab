e22bfb5de1ce887ec4301ec62c531000ca78d879
Taylor Riley <t_riley13@icloud.com>
2023-09-10 20:47:52 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(x){
+  if (x <= 400) {
+    return "This one is on me!"
+  }
+  else if (x > 400, x <= 2000) {
+    return "That will be twenty bucks."
+  }
+  else if (x > 2000, x <= 2500) {
+    return "I will gladly take your thirty bucks."
+  }
+  else if (x > 2500) {
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  const message = city === "NYC" ? "Ok, sounds good." : "No go.";  
+  return message
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let goodbye;
+  switch(tip) {
+    case "generous":
+      goodbye = "Thank you so much.";
+      break;
+    case "not as generous":
+      goodbye = "Thank you.";
+      break;
+    default:
+      goodbye = "Bye."
+      break;
+  }
+  return goodbye
 }
